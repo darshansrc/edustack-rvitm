a1ffbc9bde288c38e4909ac829aed5eeeb1fc1c1
Darshan Gowda <darshansr1618@gmail.com>
2023-11-01 15:38:11 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,75 +3,63 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { db } from "./lib/firebase-config";
 
+async function fetchUserData(userUID : string) {
+  const getRef = doc(db, 'users', userUID);
+  return await getDoc(getRef);
+}
 
+export async function middleware(request : any, response : any) {
+  const session = request.cookies.get("session");
+  const isRootPath = request.nextUrl.pathname === '/';
+  const isStudentPath = request.nextUrl.pathname.startsWith('/student');
+  const isFacultyPath = request.nextUrl.pathname.startsWith('/faculty');
 
-export async function middleware(request, response) {
-    const session = request.cookies.get("session");
-  
-    if (request.nextUrl.pathname === '/') {
-      if (session) {
-        const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
-          headers: {
-            Cookie: `session=${session?.value}`,
-          },
-        });
-  
-        if (responseAPI.status === 200) {
-          const responseBody = await responseAPI.json();
-          const userUID = responseBody.userUID;
-  
-          const getRef = doc(db, 'users', userUID);
-          const userDoc = await getDoc(getRef);
-          if (userDoc.exists()) {
-            const userData = userDoc.data();
-  
-            if (userData.type === 'student') {
-              return NextResponse.redirect(new URL('/student/home', request.url));
-            } else if (userData.type === 'faculty') {
-              return NextResponse.redirect(new URL('/faculty/home', request.url));
-            }
-          } else {
-            return NextResponse.redirect(new URL('/', request.url));
-          }
-        } else {
-          return NextResponse.redirect(new URL('/', request.url));
-        }
-      }
-    } else if (request.nextUrl.pathname.startsWith('/student') || request.nextUrl.pathname.startsWith('/faculty')) {
-      if (!session && request.nextUrl.pathname === '/') {
-        return NextResponse.rewrite(new URL('/', request.url));
-      }
-  
+  if (isRootPath) {
+    if (session) {
       const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
         headers: {
           Cookie: `session=${session?.value}`,
         },
       });
-  
+
       if (responseAPI.status === 200) {
         const responseBody = await responseAPI.json();
         const userUID = responseBody.userUID;
-  
-        const getRef = doc(db, 'users', userUID);
-        const userDoc = await getDoc(getRef);
+        const userDoc = await fetchUserData(userUID);
+
         if (userDoc.exists()) {
           const userData = userDoc.data();
-  
-          if (userData.type === 'student' && request.nextUrl.pathname.startsWith('/student')) {
-            // Allow access.
-          } else if (userData.type === 'faculty' && request.nextUrl.pathname.startsWith('/faculty')) {
-            // Allow access.
-          } else {
-            return NextResponse.redirect(new URL('/', request.url));
+          if (userData.type === 'student') {
+            return NextResponse.redirect(new URL('/student/home', request.url));
+          } else if (userData.type === 'faculty') {
+            return NextResponse.redirect(new URL('/faculty/home', request.url));
           }
-        } else {
-          return NextResponse.redirect(new URL('/', request.url));
         }
-      } else {
-        return NextResponse.redirect(new URL('/', request.url));
       }
     }
-  
-    // Handle other cases here.
+    return NextResponse.redirect(new URL('/', request.url));
+  } else if ((isStudentPath || isFacultyPath) && !session) {
+    return NextResponse.rewrite(new URL('/', request.url));
+  } else if ((isStudentPath || isFacultyPath) && session) {
+    const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
+      headers: {
+        Cookie: `session=${session?.value}`,
+      },
+    });
+
+    if (responseAPI.status === 200) {
+      const responseBody = await responseAPI.json();
+      const userUID = responseBody.userUID;
+      const userDoc = await fetchUserData(userUID);
+
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        if ((userData.type === 'student' && isStudentPath) || (userData.type === 'faculty' && isFacultyPath)) {
+          // Allow access.
+          return;
+        }
+      }
+    }
+    return NextResponse.redirect(new URL('/', request.url));
   }
-  
+}
