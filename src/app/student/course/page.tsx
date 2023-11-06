ee4cf57d9e974175032857f7927b05892f81f154
Darshan Gowda <darshansr1618@gmail.com>
2023-11-06 22:10:12 +0530

    tab

--- src/app/student/course/page.tsx
+++ src/app/student/course/page.tsx
@@ -11,122 +11,110 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 
-interface TabPanelProps {
-  children?: ReactNode;
-  index: number;
-  value: number;
+interface StyledTabProps {
+  label: string;
 }
 
-function CustomTabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+interface StyledTabsProps {
+  children?: ReactNode;
+  value: number;
+  onChange: (event: SyntheticEvent, newValue: number) => void;
+}
 
+const StyledTabs = styled((props: StyledTabsProps) => (
+  <Tabs
+    {...props}
+    variant="fullWidth"
+    scrollButtons="auto"
+    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+
+  />
+))({
+  '& .MuiTabs-indicator': {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  '& .MuiTabs-indicatorSpan': {
+    width: '100%',
+    backgroundColor: 'rgb(29 78 216)',
+  },
+  width: '100%',
+})
+
+
+const StyledTab = styled((props: StyledTabProps) => (
+  <Tab disableRipple {...props} />
+))(({ theme }) => ({
+  textTransform: 'none',
+  fontFamily: 'Poppins',
+  fontWeight: '400',
+  fontSize: theme.typography.pxToRem(14),
+  color: '#666666',
+  '&.Mui-selected': {
+    color: 'rgb(29 78 216)',
+  },
+  '&.Mui-focusVisible': {
+    backgroundColor: '#666666',
+  },
+}));
+
+const TabPanel = ({ value, index, children }) => {
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+    <div hidden={value !== index}>
+      {value === index && <Box p={3}>{children}</Box>}
     </div>
   );
-}
+};
 
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
 
 const page = () => {
 
-  const [value, setValue] = useState(0);
+  const [selectedTab, setSelectedTab] = useState(0);
 
-  const handleChange = (event: SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+  const handleTabChange = (event, newValue) => {
+    setSelectedTab(newValue);
   };
 
-  interface StyledTabProps {
-    label: string;
-  }
-  
-  interface StyledTabsProps {
-    children?: ReactNode;
-    value: number;
-    onChange: (event: SyntheticEvent, newValue: number) => void;
-  }
-  
-  const StyledTabs = styled((props: StyledTabsProps) => (
-    <Tabs
-      {...props}
-      variant="fullWidth"
-      scrollButtons="auto"
-      TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
 
-    />
-  ))({
-    '& .MuiTabs-indicator': {
-      display: 'flex',
-      justifyContent: 'center',
-      backgroundColor: 'transparent',
-    },
-    '& .MuiTabs-indicatorSpan': {
-      width: '100%',
-      backgroundColor: 'rgb(29 78 216)',
-    },
-    width: '100%',
-  })
-  
-  
-  const StyledTab = styled((props: StyledTabProps) => (
-    <Tab disableRipple {...props} />
-  ))(({ theme }) => ({
-    textTransform: 'none',
-    fontFamily: 'Poppins',
-    fontWeight: '400',
-    fontSize: theme.typography.pxToRem(14),
-    color: '#666666',
-    '&.Mui-selected': {
-      color: 'rgb(29 78 216)',
-    },
-    '&.Mui-focusVisible': {
-      backgroundColor: '#666666',
-    },
-  }));
 
   return (
     <>
     <TopNavbar name={'Course'}/>
     <Navbar/>
     <div className={styles.courseNavbar}>
-          <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <StyledTabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <StyledTab label="Item One" {...a11yProps(0)} />
-          <StyledTab label="Item Two" {...a11yProps(1)} />
-          <StyledTab label="Item Three" {...a11yProps(2)} />
-        </StyledTabs>
-      </Box>
+    <StyledTabs
+        value={selectedTab}
+        onChange={handleTabChange}
+      >
+        <StyledTab label="Tab 1" />
+        <StyledTab label="Tab 2" />
+        <StyledTab label="Tab 3" />
+      </StyledTabs>
 
-    </Box>
     </div>
         
     <div className={styles1.pageContainer} style={{paddingTop: '90px'}}>
-    <CustomTabPanel value={value} index={0}>
-        Item One
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        Item Two
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        Item Three
-      </CustomTabPanel>
+    <TabPanel value={selectedTab} index={0}>
+        <Typography variant="h6">Tab 1 Content</Typography>
+        <Typography>
+          This is the content for Tab 1. You can replace this with your actual content.
+        </Typography>
+      </TabPanel>
+
+      <TabPanel value={selectedTab} index={1}>
+        <Typography variant="h6">Tab 2 Content</Typography>
+        <Typography>
+          This is the content for Tab 2. You can replace this with your actual content.
+        </Typography>
+      </TabPanel>
+
+      <TabPanel value={selectedTab} index={3}>
+        <Typography variant="h6">Tab 3 Content</Typography>
+        <Typography>
+          This is the content for Tab 3. You can replace this with your actual content.
+        </Typography>
+      </TabPanel>
     </div>
     </>
   )
