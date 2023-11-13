d9dbc13af992ec91099b55acd471f78187767e2c
Darshan Gowda <darshansr1618@gmail.com>
2023-11-13 17:28:31 +0530

    Update AttendanceForm.tsx

--- src/app/faculty/attendance/attendance-form/AttendanceForm.tsx
+++ src/app/faculty/attendance/attendance-form/AttendanceForm.tsx
@@ -342,16 +342,7 @@ const batchFilteredStudentCards = (batch) =>
       presentCount = isSubjectElective === "compulsory" ? presentCount : electiveStudentUSN.length
   
     return (
-      <><AntModal centered title="Confirm Submission?" open={isOpen} onOk={onClose} onCancel={onClose}        footer={[
-        <AntButton key="back" onClick={onClose}>
-          Cancel
-        </AntButton>,
-        <AntButton
-        className='bg-blue-600 text-white border-white border-solid border-[1px]'
-        >
-          Submit
-        </AntButton>,
-      ]}>
+      <><AntModal centered title="Confirm Submission?" open={isOpen} onOk={onClose} onCancel={onClose}     >
 
        
        
