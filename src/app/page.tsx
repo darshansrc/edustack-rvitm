e0b8fe91ddacef723def00240d0bbb24694091c9
Darshan Gowda <darshansr1618@gmail.com>
2023-11-14 02:16:35 +0530

    home Page Redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
-'use client';
-import React from 'react';
-import styles from './HomePage.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
+"use client";
+import React from "react";
+import styles from "./HomePage.module.css";
+import LeftSideOfPage from "./HomePageComponents/LeftSideOfPage/LeftSideOfPage";
+import RightSideOfPage from "./HomePageComponents/RightSideOfPage/RightSideOfPage";
 
-const HomePage = async () => {
+const HomePage = () => {
   return (
-      <main>
-            <div className={styles.pageContainer}>              
-              <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'row' }}>
-                <Image priority  width={60} height={60} src='/logo.png' style={{ maxWidth: '60px', maxHeight: '60px', alignItems: 'center' }} alt={''}/>
-                <h1 style={{ fontSize: '25px', padding: '10px' }}> | </h1>
-                <Image priority width={60} height={60} src='/logorv.png' style={{ maxWidth: '60px', maxHeight: '60px', alignItems: 'center' }} alt={''} />
-              </div>
-              <h1 style={{ fontSize: '30px', fontWeight: 'bolder', margin: '20px', fontFamily: 'golos text', fontVariant: '600' }}>Welcome to EduStack for RVITM</h1>
-              <p style={{ margin: '20px', color: 'grey', fontFamily: 'sans-serif', marginBottom: '15vh'}}>A Platform Built to Simplify Attendance Tracking & Academics </p>
-
-              <div className={styles.buttonContainer}>
-              <Link href='/auth/signin' shallow={true} style={{width: '95%'}}><button className={styles.primaryButton}>Sign In </button></Link>
-              <button className={styles.secondaryButton}>Activate your Account</button>
-            </div>
-            </div>
-      </main>
+    <>
+      <div
+        className="h-screen flex justify-center items-center relative overflow-hidden"
+        style={{ backgroundColor: "white" }}
+      >
+        <div className={styles.pill1}></div>
+        <div className={styles.pill2}></div>
+        <div className={styles.pill3}></div>
+        <div className={styles.pill4}></div>
+        <div className=" h-3/5 w-3/4 relative flex justify-center items-center max-md:h-2/3 shadow-2xl rounded-xl max-sm:h-screen max-sm:w-screen max-sm:flex-col">
+          <LeftSideOfPage />
+          <RightSideOfPage />
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default HomePage;
