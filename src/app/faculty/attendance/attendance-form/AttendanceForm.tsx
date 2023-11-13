a68346052250acd725eeefd01505a2dd007d676d
Darshan Gowda <darshansr1618@gmail.com>
2023-11-13 13:46:04 +0530

    Attendance form

--- src/app/faculty/attendance/attendance-form/AttendanceForm.tsx
+++ src/app/faculty/attendance/attendance-form/AttendanceForm.tsx
@@ -5,6 +5,8 @@ import TopNavbar from '@/app/student/components/topnavbar/TopNavbar';
 import StudentCard from './studentcard/StudentCard';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '@/lib/firebase-config';
+import styles from './AttendanceForm.module.css'
+import { IoChevronBackSharp } from 'react-icons/io5';
 
 interface AttendanceFormData {
   classId: string;
@@ -303,6 +305,8 @@ const batchFilteredStudentCards = (batch) =>
 
   const stepOne = () => {
     return (
+      <>
+      <TopNavbar name='Mark Attendance' /> 
       <div className='flex items-center justify-center flex-col w-[100vw] min-h-[100vh] '>
         <div className='flex items-center flex-col bg-white rounded-xl border-solid border w-[90vw] max-w-[500px] border-slate-300 p-4'>
           <h2 className='text-center font-[Poppins] font-[500] text-xl p-2 my-6 text-blue-600'> Mark Attendance</h2>
@@ -373,13 +377,13 @@ const batchFilteredStudentCards = (batch) =>
 
             <div className=' w-[80vw] max-w-[450px] flex flex-row justify-between mb-4 text-[16px]'>
 
-              <div >
-            <p className='text-left flex whitespace-nowrap font-[Poppins] mt-5 font-[500] text-[12px] pl-2 text-slate-600 w-[39vw] max-w-[220px]  mr-[1vw]'>Start Time</p>
+              <div className='w-full'>
+            <p className='text-left flex whitespace-nowrap font-[Poppins] mt-5 font-[500] text-[12px] pl-2 text-slate-600  '>Start Time</p>
               <Select
                 size='large'
                 value={classStartTime || undefined}
                 onChange={handleStartTimeChange}
-                className=' w-[39vw] max-w-[220px]   mr-[1vw] text-black text-[16px] mt-[2px]'
+                className='w-[40vw] max-w-[225px] pr-[5px]  text-[16px] mt-[2px]'
                 placeholder='Select Start Time'
               >
                 {timeOptions.map((option) => (
@@ -390,14 +394,14 @@ const batchFilteredStudentCards = (batch) =>
               </Select>
               </div>
 
-              <div >
-              <p className='text-left whitespace-nowrap font-[Poppins] font-[500] text-[12px] mt-5 ml-4 text-slate-600 w-[39vw] max-w-[220px]'>End Time</p>
+              <div className='w-full'>
+              <p className='text-left whitespace-nowrap font-[Poppins] font-[500] text-[12px] mt-5 pl-3  text-slate-600 '>End Time</p>
               <Select
                 size='large'
                 value={classEndTime || undefined}
                 onChange={(value) => setClassEndTime(value)}
                 placeholder='Select End Time'
-                className=' w-[39vw] max-w-[220px] ml-[1vw] text-black text-[16px] mt-[2px]'
+                className='w-[40vw] max-w-[225px] pl-[5px] text-[16px] mt-[2px]'
               >
                 {timeOptions.map((option) => (
                   <Select.Option key={option.value} value={option.value}>
@@ -422,59 +426,50 @@ const batchFilteredStudentCards = (batch) =>
           </div>
         </div>
       </div>
+      </>
     );
   };
 
   const stepTwo = () => {
     return (
       <div
-      className="mainContainer"
-      style={{
-        overflow: "hidden",
-        marginBottom: "160px",
-        marginTop: "100px",
-      }}
+      className={styles.cardContainer}
+  
     >
-      <h3 style={{ paddingBottom: "10px", textAlign: "center" }}>
-        Mark Attendance
-      </h3>
-      <h6
-        style={{
-          paddingBottom: "10px",
-          marginLeft: "20px",
-          marginRight: "20px",
-          textAlign: "center",
-          fontWeight: "400",
-          color: "#777",
-        }}
-      >
-        By Default All the Students are Marked as Present, Please tap on the
-        cards to make changes, confirm the Absentees and submit the form.{" "}
-      </h6>
-      <h6 style={{ paddingBottom: "15px" }}>
-        [&nbsp;<span className="text-present"> P </span>&nbsp;-
-        Present,&nbsp;&nbsp;<span className="text-absent"> A </span>&nbsp;-
-        Absent&nbsp;]{" "}
-      </h6>
-      <div>
-      <div>
-    {labBatch === null && filteredStudentCards}
+
+      <div className={styles.cardTopBar}>
+        <button onClick={() => setFormStep(1)} className='absolute left-0 p-2 m-4 bg-slate-50 rounded-[20px]'>
+        <IoChevronBackSharp/>
+        </button>
+        
+       <h4 className='font-[Poppins] text-slate-800 font-[500]  my-4'>Mark Attendance</h4> 
+        
+      </div>
+
+      <div className='py-[150px]'>
+    {!labBatch && filteredStudentCards}
     {labBatch && batchFilteredStudentCards(labBatch)}
       </div>
+
+        <div className={styles.submitButtonContainer} >
         <button
-              onClick={handleStep1Submit}
-              className='bg-blue-500 w-full text-white rounded-lg mx-4 mt-4 mb-4 font-[Poppins] p-2 hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300'
-            >
-              SUBMIT
-            </button>
-            <button
-              onClick={handleStep1Submit}
-              className='bg-white w-full text-blue-500 rounded-lg mx-4 mt-4 mb-4 font-[Poppins] p-2 hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300'
+              onClick={() => setFormStep(1)}
+              className='bg-slate-100 w-[40vw]  max-w-[180px] text-blue-500 rounded-[15px] mx-2 mt-2 mb-2 font-[Poppins] p-2 px-4 hover:bg-slate-200 focus:outline-none focus:ring focus:ring-blue-300'
             >
               BACK
-            </button>
+        </button>
+        <button
+              onClick={handleStep1Submit} 
+              className='bg-blue-500 w-[40vw] max-w-[180px] text-white rounded-[15px] mx-2  mt-2 mb-2 font-[Poppins] p-2 px-4 hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300'
+            >
+              SUBMIT
+         </button>
+
+        </div>
+
+
       </div>
-      </div>
+     
     );
   };
 
@@ -489,7 +484,10 @@ const batchFilteredStudentCards = (batch) =>
 
   return (
     <div>
-     <TopNavbar name='Mark Attendance' /> 
+   
+     
+     
+     
       {formStep === 1 && stepOne()}
       {formStep === 2 && stepTwo()}
       {formStep === 3 && stepThree()}
--- src/app/faculty/attendance/attendance-form/studentcard/StudentCard.tsx
+++ src/app/faculty/attendance/attendance-form/studentcard/StudentCard.tsx
@@ -4,6 +4,7 @@ export default function StudentCard(props) {
   const attendanceStatus = props.Present ? "present" : "absent";
   const slNo = Number(props.USN.toString().replace(/^0+/, '').slice(-3));
   return (
+    
     <div className={`cardd ${attendanceStatus}`} onClick={props.toggle} style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
           <div >
           <h6 className="cardd__number">{slNo + '.'}</h6>
@@ -15,6 +16,7 @@ export default function StudentCard(props) {
         <h2 className="text-[14px] font-[500] font-[Poppins]" style={{textTransform: 'capitalize'}}>{props.Name}</h2>
         <p className="text-[12px] text-[#333] font-[Poppins]">{props.USN.toString()}</p>
       </div>
+
       <div>
         {props.Present ? (
           <span className="text-white bg-[green] rounded-[50%] w-[25px] h-[25px] flex items-center justify-center">P</span>
