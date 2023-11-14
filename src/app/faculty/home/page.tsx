2e0c7c3e0988b0fad0d5f4df1f862d6c54f4e31d
Darshan Gowda <darshansr1618@gmail.com>
2023-11-14 21:09:43 +0530

    Update page.tsx

--- src/app/faculty/home/page.tsx
+++ src/app/faculty/home/page.tsx
@@ -1,14 +1,13 @@
 'use client';
-import getUser from '@/lib/getUser'
+
 import React from 'react'
 import BottomNavbar from '../components/bottomNavbar/BottomNavbar';
 import TopNavbar from '@/app/student/components/topnavbar/TopNavbar';
 import FacultyHomePage from './FacultyHomePage';
 
 async function App() {
-  const user = await getUser()
 
-  const formattedString = JSON.stringify(user, null, "\t");
+
 
   return (
     <>
