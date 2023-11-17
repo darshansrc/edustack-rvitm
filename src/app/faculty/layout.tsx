4513896590247944ea1e5ab86162ffdd7167d095
Darshan Gowda <darshansr1618@gmail.com>
2023-11-17 10:25:00 +0530

    top loading bar

--- src/app/faculty/layout.tsx
+++ src/app/faculty/layout.tsx
@@ -1,11 +1,18 @@
+'use client;'
+import { useRef } from "react"
 import DesktopNavbar from "./components/DesktopNavBar/DesktopNavbar"
 import BottomNavbar from "./components/bottomNavbar/BottomNavbar"
+import LoadingBar from 'react-top-loading-bar'
+
 
 export default function DashboardLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
+
+    const ref = useRef(null)
+
     return (
         <>
         <DesktopNavbar/>
