30b23db34d13ae46fe8b19128725360b67328695
Darshan Gowda <darshansr1618@gmail.com>
2023-11-21 11:29:46 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,33 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
+import { db } from "@/lib/firebase-config";
+import { customInitApp } from "@/lib/firebase-admin-config";
+import { doc, getDoc } from "firebase/firestore";
+import { auth } from "firebase-admin";
+
+customInitApp();
 
 export async function middleware(request: NextRequest, response: NextResponse) {
-  const session = request.cookies.get("session");
+  let userType: string = "";
+
+  const session = cookies().get("session")?.value || "";
+
+  const decodedClaims = await auth().verifySessionCookie(session, true);
+
+  if (!decodedClaims) {
+    return NextResponse.json({ isLogged: false }, { status: 401 });
+  }
+
+  const userUID = decodedClaims.uid;
+  const getRef = doc(db, "users", userUID);
+  const userDoc = await getDoc(getRef);
+
+  if (userDoc.exists()) {
+    const userData = userDoc.data();
+    userType = userData.type;
+  }
+
   const { pathname } = request.nextUrl;
 
   if (pathname === "/" && !session) {
@@ -11,20 +35,10 @@ export async function middleware(request: NextRequest, response: NextResponse) {
   }
 
   if (pathname === "/" && session) {
-    const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
-      headers: {
-        Cookie: `session=${session?.value}`,
-      },
-    });
-
-    if (responseAPI.status === 200) {
-      const { userType } = await responseAPI.json();
-
-      if (userType === "student") {
-        return NextResponse.redirect(new URL("/student/home", request.url));
-      } else if (userType === "faculty") {
-        return NextResponse.redirect(new URL("/faculty/home", request.url));
-      }
+    if (userType === "student") {
+      return NextResponse.redirect(new URL("/student/home", request.url));
+    } else if (userType === "faculty") {
+      return NextResponse.redirect(new URL("/faculty/home", request.url));
     }
 
     return NextResponse.redirect(new URL("/", request.url));
@@ -35,25 +49,15 @@ export async function middleware(request: NextRequest, response: NextResponse) {
       return NextResponse.rewrite(new URL("/auth/signin", request.url));
     }
 
-    const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
-      headers: {
-        Cookie: `session=${session?.value}`,
-      },
-    });
-
-    if (responseAPI.status === 200) {
-      const { userType } = await responseAPI.json();
-
-      if (
-        (userType === "student" && pathname.startsWith("/student")) ||
-        (userType === "faculty" && pathname.startsWith("/faculty"))
-      ) {
-        // Allow access.
-      } else {
-        return NextResponse.redirect(new URL("/auth/signin", request.url));
-      }
+    if (
+      (userType === "student" && pathname.startsWith("/student")) ||
+      (userType === "faculty" && pathname.startsWith("/faculty"))
+    ) {
+      // Allow access.
     } else {
       return NextResponse.redirect(new URL("/auth/signin", request.url));
     }
+  } else {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
   }
 }
