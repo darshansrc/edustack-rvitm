c0b8e9f86954c1243db96c743f5d3325be6e43f7
Darshan Gowda <darshansr1618@gmail.com>
2023-11-24 23:25:51 +0530

    all middleware bug fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,50 +1,88 @@
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-async function checkAuthentication(request, response) {
+export async function middleware(request: NextRequest, response: NextResponse) {
   const session = request.cookies.get("session");
   const { pathname } = request.nextUrl;
 
-  try {
-    const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
-      headers: { Cookie: `session=${session?.value}` },
-    });
+  if (pathname === "/" && !session) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
 
-    if (responseAPI.status === 200) {
-      const { userType } = await responseAPI.json();
+  if (pathname === "/" && session) {
+    try {
+      const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
+        headers: { Cookie: `session=${session?.value}` },
+      });
 
-      if (!session) {
+      if (responseAPI.status === 200) {
+        const { userType } = await responseAPI.json();
+
+        if (userType === "student") {
+          return NextResponse.redirect(new URL("/student/home", request.url));
+        } else if (userType === "faculty") {
+          return NextResponse.redirect(new URL("/faculty/home", request.url));
+        }
+      } else {
         return NextResponse.redirect(new URL("/auth/signin", request.url));
       }
-
-      if (
-        (userType === "faculty" && pathname.startsWith("/faculty")) ||
-        (userType === "student" && pathname.startsWith("/student"))
-      ) {
-        // Continue to the protected route
-      } else {
-        return NextResponse.redirect(new URL("/", request.url));
-      }
-    } else {
-      handleFailedAuthentication(request, response, session);
+    } catch (error) {
+      console.error("Error checking authentication API:", error);
+      return NextResponse.redirect(new URL("/auth/signin", request.url));
     }
-  } catch (error) {
-    console.error("Error checking authentication API:", error);
-    handleFailedAuthentication(request, response, session);
+  }
+
+  if (pathname.startsWith("/student") || pathname.startsWith("/faculty")) {
+    if (!session) {
+      return NextResponse.rewrite(new URL("/auth/signin", request.url));
+    }
+
+    try {
+      const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
+        headers: { Cookie: `session=${session?.value}` },
+      });
+
+      if (responseAPI.status === 200) {
+        const { userType } = await responseAPI.json();
+
+        if (
+          (userType === "student" && pathname.startsWith("/student")) ||
+          (userType === "faculty" && pathname.startsWith("/faculty"))
+        ) {
+          // Continue to the protected route
+        } else {
+          return NextResponse.redirect(new URL("/auth/signin", request.url));
+        }
+      } else {
+        return NextResponse.redirect(new URL("/auth/signin", request.url));
+      }
+    } catch (error) {
+      console.error("Error checking authentication API backward:", error);
+
+      return NextResponse.redirect(new URL("/auth/signin", request.url));
+    }
+  }
+
+  if (pathname === "/auth/signin" && session) {
+    try {
+      const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
+        headers: { Cookie: `session=${session?.value}` },
+      });
+
+      if (responseAPI.status === 200) {
+        const { userType } = await responseAPI.json();
+
+        if (userType === "student") {
+          return NextResponse.redirect(new URL("/student/home", request.url));
+        } else if (userType === "faculty") {
+          return NextResponse.redirect(new URL("/faculty/home", request.url));
+        }
+      }
+    } catch (error) {
+      console.error("Error checking authentication API at /auth/signin", error);
+    }
   }
 
   // Continue to the next middleware or route handler
   return NextResponse.next();
-}
-
-function handleFailedAuthentication(request, response, session) {
-  // Delete the session cookie on failed authentication
-  if (response.cookies && session) {
-    response.cookies.delete("session");
-    console.log("Session cookie deleted  middleware!");
-  }
-  return NextResponse.redirect(new URL("/auth/signin", request.url));
-}
-
-export async function middleware(request, response) {
-  return checkAuthentication(request, response);
 }
