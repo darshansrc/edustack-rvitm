024f72bdb486ce113d39fd6b7342d5666849727c
Darshan Gowda <darshansr1618@gmail.com>
2023-11-24 22:37:22 +0530

    update

--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -1,56 +1,11 @@
-"use client";
 import DesktopNavbar from "./components/desktopnavbar/DesktopNavBar";
 import Navbar from "./components/navbar/Navbar";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth, db } from "@/lib/firebase-config";
-import { useRouter } from "next/navigation";
-import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [user, setUser] = useState<any>({});
-  const router = useRouter();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
-      console.log("Auth", currentuser);
-      setUser(currentuser);
-      console.log(user);
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, [user]);
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      const getRef = doc(db, "users", user.uid);
-      const userDoc = await getDoc(getRef);
-
-      try {
-        if (userDoc.exists()) {
-          const userData = userDoc.data();
-          if (userData.type === "student") {
-            //  router.push("/student/home");
-          } else if (userData.type === "faculty") {
-            router.push("/faculty/home");
-          } else if (userData.type === "parent") {
-            router.push("/parent/home");
-          } else {
-            await signOut(auth);
-          }
-        }
-      } catch (err) {
-        await signOut(auth);
-      }
-    };
-    checkAuth();
-  }, []);
   return (
     <>
       <Navbar />
